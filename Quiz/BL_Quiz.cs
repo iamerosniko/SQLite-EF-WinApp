b774ec12df0904fc6bfa5ca6cc0409be11d81994
Eros Niko Cas Alvarez <iamerosniko@yahoo.com>
2017-03-19 22:29:49 +0800

    up

---
+++ Quiz/BL_Quiz.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz
+{
+    public class BL_Quiz
+    {
+        QuizEntities db = new QuizEntities();
+        public IQueryable<Question> GetQuestion()
+        {
+            var q = from l in db.Questions
+                    select l;
+            return q;
+        }
+
+        public void SaveResults(Student stud)
+        {
+            db.Entry(stud).State = EntityState.Added;
+            db.SaveChanges();
+        }
+    }
+}
